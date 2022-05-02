962fe703f6b6f6e1fda768f8d749f72d12dffb76
Bashie <ashenarya3@yahoo.com>
2022-05-02 20:20:15 -0300

    agregado estado

--- models/pedido.js
+++ models/pedido.js
@@ -3,7 +3,8 @@ const pedidoSchema = new mongoose.Schema({
 	"cliente": [
 		{ type: mongoose.Schema.Types.ObjectId, ref: 'cliente' }
 	],
-	"items": String
+	"items": String,
+	"estado": String
 }, { timestamps: true });
 const Pedido = mongoose.model('pedido', pedidoSchema);
 export default Pedido;
