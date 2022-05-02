61db0f24e88e33d24ff60398971fc9873149bf12
Bashie <ashenarya3@yahoo.com>
2022-05-02 19:07:57 -0300

    Agregada pk
    Signed-off-by: Bashie <ashenarya3@yahoo.com>

--- models/pedido.js
+++ models/pedido.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 const pedidoSchema = new mongoose.Schema({
-	"dni": String,
+	"cliente": [
+		{ type: mongoose.Schema.Types.ObjectId, ref: 'cliente' }
+	],
 	"items": String
 }, { timestamps: true });
 const Pedido = mongoose.model('pedido', pedidoSchema);
