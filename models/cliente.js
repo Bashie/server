9d3f49fb294d48dae33e03d034bf73f4c882a4e3
Bashie <ashenarya3@yahoo.com>
2022-05-02 18:36:22 -0300

    agregado nickname
    Signed-off-by: Bashie <ashenarya3@yahoo.com>

--- models/cliente.js
+++ models/cliente.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 const clienteSchema = new mongoose.Schema({
 	"nombre": String,
 	"apellido": String,
+	"nickname": String,
 	"dni": String,
 }, { timestamps: true });
 const Cliente = mongoose.model('cliente', clienteSchema);
